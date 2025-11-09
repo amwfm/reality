ef11bc29055523d9ecf35ad2a75009df1b0fe3dd
cmcnmn <153211547+cmcnmn@users.noreply.github.com>
2025-11-09 21:47:44 +0800

    Update tuic.sh

--- tuic.sh
+++ tuic.sh
@@ -85,7 +85,7 @@ if [ -d "/root/tuic" ]; then
             public_ip=$(curl -s https://api.ipify.org)
             isp=$(curl -s https://speed.cloudflare.com/meta | awk -F\" '{print $26"-"$18}' | sed -e 's/ /_/g')
             echo -e "\e[1;33m\nV2rayN、NekoBox\e[0m"
-            echo -e "\e[1;32mtuic://$new_uuid:$password@$public_ip:$new_port?congestion_control=bbr&alpn=h3&sni=www.bing.com&udp_relay_mode=native&allow_insecure=1#$isp\e[0m"
+            echo -e "\e[1;32mtuic://$new_uuid:$password@$public_ip:$new_port?congestion_control=bbr&alpn=h3&sni=touch.10086.cn&udp_relay_mode=native&allow_insecure=1#$isp\e[0m"
             echo ""
             exit 0
             ;;
@@ -152,7 +152,7 @@ fi
 chmod 755 tuic-server
 
 # Create self-signed certs
-openssl req -x509 -nodes -newkey ec:<(openssl ecparam -name prime256v1) -keyout /root/tuic/server.key -out /root/tuic/server.crt -subj "/CN=bing.com" -days 36500
+openssl req -x509 -nodes -newkey ec:<(openssl ecparam -name prime256v1) -keyout /root/tuic/server.key -out /root/tuic/server.crt -subj "/CN=10086.cn" -days 36500
 
 # Prompt user for port and password
 echo ""
@@ -234,5 +234,5 @@ isp=$(curl -s https://speed.cloudflare.com/meta | awk -F\" '{print $26"-"$18}' |
 
 # nekoray/nekobox URL
 echo -e "\e[1;33m\nV2rayN、NekoBox\e[0m"
-echo -e "\e[1;32mtuic://$UUID:$password@$public_ip:$port?congestion_control=bbr&alpn=h3&sni=www.bing.com&udp_relay_mode=native&allow_insecure=1#$isp\e[0m"
+echo -e "\e[1;32mtuic://$UUID:$password@$public_ip:$port?congestion_control=bbr&alpn=h3&sni=touch.10086.cn&udp_relay_mode=native&allow_insecure=1#$isp\e[0m"
 echo ""
