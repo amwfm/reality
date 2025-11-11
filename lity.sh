cd29a70a84085d322181c416871dce7805cde81f
cmcnmn <153211547+cmcnmn@users.noreply.github.com>
2025-11-12 00:44:35 +0800

    Update lity.sh

--- lity.sh
+++ lity.sh
@@ -102,7 +102,7 @@ reconfig() {
                     "dest": "1.1.1.1:443",
                     "xver": 0,
                     "serverNames": [
-                        "2e2bfe1b3e4abc25589df0413ba22c58.tvmonitor.chinaunicomvideo.cn"
+                        "uni-tjtj-mce-01-002.wo186.tv"
                     ],
                     "privateKey": "$rePrivateKey",
                     "minClientVer": "",
@@ -137,7 +137,7 @@ EOF
     # 删除运行脚本
     rm -f tcp-wss.sh install-release.sh reality.sh 
     IP=$(getIP)
-    url="vless://${UUID}@${IP}:${PORT}?encryption=none&flow=xtls-rprx-vision&security=reality&sni=2e2bfe1b3e4abc25589df0413ba22c58.tvmonitor.chinaunicomvideo.cn&fp=chrome&pbk=${rePublicKey}&sid=${shortId}&type=tcp&headerType=none#$ISP"
+    url="vless://${UUID}@${IP}:${PORT}?encryption=none&flow=xtls-rprx-vision&security=reality&sni=uni-tjtj-mce-01-002.wo186.tv&fp=chrome&pbk=${rePublicKey}&sid=${shortId}&type=tcp&headerType=none#$ISP"
 
     echo ""
     echo -e "\e[1;32mreality 安装成功\033[0m"
