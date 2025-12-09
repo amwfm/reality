e255dd6496bbf9a5cc3e9d4db653b288be6c8a1d
cmcnmn <153211547+cmcnmn@users.noreply.github.com>
2025-12-09 20:02:26 +0800

    Update reality.sh

--- reality.sh
+++ reality.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 export PORT=${PORT:-'8880'}
 export UUID=${UUID:-$(cat /proc/sys/kernel/random/uuid)}
-export SNI=${SNI:-$}
 
 # 检查是否为root下运行
 [[ $EUID -ne 0 ]] && echo -e '\033[1;35m请在root用户下运行脚本\033[0m' && sleep 1 && exit 1
@@ -102,8 +101,8 @@ reconfig() {
                     "show": false,
                     "dest": "1.1.1.1:443",
                     "xver": 0,
-                    "SNI": [
-                        "$SNI"
+                    "serverNames": [
+                        "shoutingtoutiao3.10010.com"
                     ],
                     "privateKey": "$rePrivateKey",
                     "minClientVer": "",
@@ -138,7 +137,7 @@ EOF
     # 删除运行脚本
     rm -f tcp-wss.sh install-release.sh reality.sh 
     IP=$(getIP)
-    url="vless://${UUID}@${IP}:${PORT}?encryption=none&flow=xtls-rprx-vision&security=reality&${SNI}&fp=chrome&pbk=${rePublicKey}&sid=${shortId}&type=tcp&headerType=none#$ISP"
+    url="vless://${UUID}@${IP}:${PORT}?encryption=none&flow=xtls-rprx-vision&security=reality&sni=shoutingtoutiao3.10010.com&fp=chrome&pbk=${rePublicKey}&sid=${shortId}&type=tcp&headerType=none#$ISP"
 
     echo ""
     echo -e "\e[1;32mreality 安装成功\033[0m"
